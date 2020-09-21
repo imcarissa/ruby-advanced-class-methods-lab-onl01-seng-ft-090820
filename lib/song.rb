a23810c85c40ef1cc654c086a127c376deb3c521
Carissa Sarreal <carissa.sarreal@gmail.com>
2020-09-21 13:25:25 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -50,5 +50,11 @@ class Song
     song
   end
   
-  
+  def self.create_from_filename(filename)
+    split_filename = filename.chop.chop.chop.chop.split(" - ")
+    song = Song.new
+    song.name = split_filename[1]
+    song.artist_name = split_filename[0]
+    song
+  end
 end
